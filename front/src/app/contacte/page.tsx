222a94aab752023f25efa0fc2bdbb11d022f5d7f
Àngel <iamamc3@gmail.com>
2024-08-13 00:32:25 +0200

    Update page.tsx

--- front/src/app/contacte/page.tsx
+++ front/src/app/contacte/page.tsx
@@ -3,7 +3,7 @@ export default function Contacte() {
         <main className="px-4 mx-auto my-12 max-w-6xl">
             <div className="space-y-6">
                 <h2 className="text-2xl font-bold">Contacte</h2>
-                <p className="text-lg">Qualsevol cosa contacteu per Matrix a @angeldenom:matrix.org</p>
+                <p className="text-lg">Qualsevol cosa contacteu per Matrix a <a href="https://matrix.to/#/@angeldenom:matrix.org" className="text-gray-500 underline">@angeldenom:matrix.org</a></p>
             </div>
         </main>
     )
